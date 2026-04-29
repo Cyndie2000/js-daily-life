879130c1d738c046a1658f847f0d3908de6a12c7
Cyndie2000 <cyndiedarlene8@gmail.com>
2026-04-29 20:07:23 +0300

    Refactor dailyLife.js by removing comments

--- Desktop/js-daily-life/dailyLife.js
+++ Desktop/js-daily-life/dailyLife.js
@@ -1,24 +1,19 @@
-// Part 1: Variables and Data Types
 let name = "Cyndie";
 let age = 18;
 let favoriteFood = "rice and chicken";
 let studyHoursPerDay = 1;
 
-// Bonus
 let isStudent = true;
 
-// Part 2: Simple Calculations
 let studyHoursPerWeek = studyHoursPerDay * 7;
 let ageInMonths = age * 12;
 let ageInDogYears = age * 7;
 
-// Part 3: Output
 console.log("Hello, my name is " + name + ".");
 console.log("I am " + age + " years old.");
 console.log("I love eating " + favoriteFood + ".");
 console.log("I study " + studyHoursPerDay + " hours per day, which is " + studyHoursPerWeek + " hours per week.");
 console.log("I am " + ageInMonths + " months old.");
 
-// Bonus Output
 console.log("In dog years, I am " + ageInDogYears + " years old.");
 console.log("Am I a student? " + isStudent);
